7d28a8e63733e95889ca004909619854e4e335a2
giao哥 <1845020345@qq.com>
2021-07-11 11:21:56 +0800

    upload

--- tf.py
+++ tf.py
@@ -4,9 +4,6 @@ from test2.lr_utils import load_dataset
 import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
-    a = np.array([[1, 3, 4]])
-    train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes = load_dataset()
-
-    index = 24
-    plt.imshow(train_set_x_orig[index])
-    print("y={} is a {} picture".format(train_set_y_orig[:, index], classes[np.squeeze(train_set_y_orig[:, index])].decode("utf-8")))
+     a = np.random.randn(2, 3, 4)
+     b = a.reshape(2, -1)
+     print(b)
